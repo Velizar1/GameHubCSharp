97c6ffbf1a269f439fe7fc88fae1a77984a9f518
julyan97 <julyan997@gmail.com>
2021-01-20 22:59:57 +0200

    Added AccountController

---
+++ GameHubCSharp/Controllers/AccountController.cs
@@ -0,0 +1,109 @@
+﻿using GameHubCSharp.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameHubCSharp.Controllers
+{
+    public class RegModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
+
+    }
+    public class AccountController : Controller
+    {
+        private SignInManager<User> _signManager;
+        private UserManager<User> _userManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signManager)
+        {
+            _userManager = userManager;
+            _signManager = signManager;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.UserName };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "")
+        {
+            var model = new LoginViewModel { ReturnUrl = returnUrl };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signManager.PasswordSignInAsync(model.Username,
+                   model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid login attempt");
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _signManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
