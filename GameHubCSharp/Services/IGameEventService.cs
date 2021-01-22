bcb843fc68a9f37acad00fc4cf8dfce56e676aee
v.d.vrubchev <v.d.vrubchev@gmail.com>
2021-01-22 16:41:43 +0200

    service layer added

--- GameHubCSharp/Services/IGameEventService.cs
+++ GameHubCSharp/Services/IGameEventService.cs
@@ -1,6 +1,12 @@
-﻿namespace GameHubCSharp.Services
+﻿using GameHubCSharp.Data.Models;
+using System.Collections.Generic;
+
+namespace GameHubCSharp.Services
 {
     public interface IGameEventService<T>
     {
+        public ICollection<GameEvent> FindEventsByGame(string game);
+
+        public GameEvent FindEventsById(string id);
     }
 }
